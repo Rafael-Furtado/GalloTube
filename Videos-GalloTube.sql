6208ce94c0cc015d99aa5636e84a0a851e3b3cfd
furtado <rafaelfurt28@gmail.com>
2023-09-01 11:06:16 -0300

    Alteração no banco de dados

--- Videos-GalloTube.sql
+++ Videos-GalloTube.sql
@@ -33,12 +33,12 @@ INSERT INTO Tag(Id, Name) VALUES
 (30,'Familia');
 
 INSERT INTO Video(Id, Name, Description, UploadDate, Duration, Thumbnail) VALUES
-( 1, 'The Shawshank Redemption', 'Dois homens presos se reúnem ao longo de vários anos, encontrando consolo e eventual redenção através de atos de decência comum.', 1994, 142, '\\img\\videos\\01.jpg'),
-( 2, 'O Poderoso Chefão', 'O patriarca idoso de uma dinastia do crime organizado transfere o controle de seu império clandestino para seu filho relutante.', 1972, 175, '\\img\\videos\\02.jpg'),
-( 3, 'Batman: O Cavaleiro das Trevas', 'Quando a ameaça conhecida como O Coringa surge de seu passado, causa estragos e caos nas pessoas de Gotham. O Cavaleiro das Trevas deve aceitar um dos maiores testes para combater a injustiça.', 2008, 152, '\\img\\videos\\03.jpg'),
-( 4, 'O Poderoso Chefão II', 'Em 1950, Michael Corleone, agora à frente da família, tenta expandir o negócio do crime a Las Vegas, Los Angeles e Cuba. Paralelamente, é revelada a história de Vito Corleone, e de como saiu da Sicília e chegou a Nova Iorque.', 1974, 202, '\\img\\videos\\04.jpg'),
-( 5, '12 Homens e uma Sentença', 'Um jurado que se aposenta tenta evitar um erro judicial forçando seus colegas a reconsiderarem as evidências.', 1957, 96, '\\img\\videos\\05.jpg'),
-( 6, 'A Lista de Schindler', 'Depois de testemunhar a perseguição dos judaicos na Polônia ocupada pelos alemães durante a Segunda Guerra Mundial, o industrial Oskar Schindler se começa a preocupar com sua força de trabalho judaica.', 1993, 195, '\\img\\videos\\06.jpg');
+( 1, 'The Shawshank Redemption', 'Dois homens presos se reúnem ao longo de vários anos, encontrando consolo e eventual redenção através de atos de decência comum.', '2006/07/05', 142, '\\img\\videos\\01.jpg'),
+( 2, 'O Poderoso Chefão', 'O patriarca idoso de uma dinastia do crime organizado transfere o controle de seu império clandestino para seu filho relutante.', '2006/07/05', 175, '\\img\\videos\\02.jpg'),
+( 3, 'Batman: O Cavaleiro das Trevas', 'Quando a ameaça conhecida como O Coringa surge de seu passado, causa estragos e caos nas pessoas de Gotham. O Cavaleiro das Trevas deve aceitar um dos maiores testes para combater a injustiça.', '2006/07/05', 152, '\\img\\videos\\03.jpg'),
+( 4, 'O Poderoso Chefão II', 'Em 1950, Michael Corleone, agora à frente da família, tenta expandir o negócio do crime a Las Vegas, Los Angeles e Cuba. Paralelamente, é revelada a história de Vito Corleone, e de como saiu da Sicília e chegou a Nova Iorque.', '2006/07/05', 202, '\\img\\videos\\04.jpg'),
+( 5, '12 Homens e uma Sentença', 'Um jurado que se aposenta tenta evitar um erro judicial forçando seus colegas a reconsiderarem as evidências.', '2006/07/05', 96, '\\img\\videos\\05.jpg'),
+( 6, 'A Lista de Schindler', 'Depois de testemunhar a perseguição dos judaicos na Polônia ocupada pelos alemães durante a Segunda Guerra Mundial, o industrial Oskar Schindler se começa a preocupar com sua força de trabalho judaica.', '2006/07/05', 195, '\\img\\videos\\06.jpg');
 
 
 
